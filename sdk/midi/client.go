b574152848d68be0e39a18bc0f06b9d41d5f6557
Leandro Ferreira <leandro.dferreira@mercadolivre.com>
2024-10-30 12:33:14 -0300

    feat(midi): add NewMIDIClient function to initialize MIDI client with options

---
+++ sdk/midi/client.go
@@ -0,0 +1,28 @@
+package midi
+
+import (
+	"github.com/leandrodaf/midi/sdk/contracts"
+)
+
+// NewMIDIClient creates a new MIDI client with the specified options.
+// It applies default options and initializes the client.
+//
+// opts ...contracts.Option: A variadic list of option functions to customize the client configuration.
+//
+// Returns:
+//   - contracts.ClientMIDI: An instance of the MIDI client.
+//   - error: An error, if any occurred during the creation of the client.
+func NewMIDIClient(opts ...contracts.Option) (contracts.ClientMIDI, error) {
+	options, err := applyDefaultOptions(opts...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := NewClient(&options)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
